ab0d5d02b6bfb5f1b790c842e328e9cb2a20a68e
Rohit Verma <verma.rohit.in@gmail.com>
2022-04-26 23:29:24 +0530

    fix: more instructions added

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-cd "G:/Github Projects/Daily-Git-Commit-main" & "C:/Users/verma/AppData/Local/Programs/Python/Python310/python.exe" "G:/Github Projects/Daily-Git-Commit-main/main.py"
+cd "C:/Daily-Git-Commit-main" & "C:/Users/verma/AppData/Local/Programs/Python/Python310/python.exe" "C:/Daily-Git-Commit-main/main.py"
